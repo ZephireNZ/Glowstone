33b684b8f36a0ae5c0e9fe9dbf4091a7fd2cf901
ZephireNZ <brynleymcdonald@gmail.com>
2014-09-13 14:54:38 +1200

    Changes to storage of Profiles - Player profile is now saved and loaded, along with textures etc. (not yet tested) - Not sure how Vanilla implementation gets profile for offline players etc - Moved some static methods to BlockSkull

--- src/main/java/net/glowstone/block/blocktype/BlockSkull.java
+++ src/main/java/net/glowstone/block/blocktype/BlockSkull.java
@@ -1,12 +1,15 @@
 package net.glowstone.block.blocktype;
 
 import net.glowstone.GlowChunk;
+import net.glowstone.GlowServer;
 import net.glowstone.block.GlowBlock;
 import net.glowstone.block.GlowBlockState;
 import net.glowstone.block.entity.TESkull;
 import net.glowstone.block.entity.TileEntity;
 import net.glowstone.block.state.GlowSkull;
 import net.glowstone.entity.GlowPlayer;
+import net.glowstone.entity.meta.PlayerProfile;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.SkullType;
@@ -19,8 +22,11 @@ import org.bukkit.util.Vector;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.UUID;
 
 public class BlockSkull extends BlockType {
+
+    public static final int MAX_OWNER_LENGTH = 16;
 
     public BlockSkull() {
         setDrops(new ItemStack(Material.SKULL_ITEM));
@@ -46,7 +52,7 @@ public class BlockSkull extends BlockType {
     @Override
     public void afterPlace(GlowPlayer player, GlowBlock block, ItemStack holding) {
         GlowSkull skull = (GlowSkull) block.getState();
-        skull.setSkullType(GlowSkull.getType(holding.getDurability()));
+        skull.setSkullType(getType(holding.getDurability()));
         if(skull.getSkullType() == SkullType.PLAYER) {
             SkullMeta meta = (SkullMeta) holding.getItemMeta();
             if(meta != null) {
@@ -59,7 +65,7 @@ public class BlockSkull extends BlockType {
             // 22.5 = 360 / 16
             long facing = Math.round(loc.getYaw() / 22.5) + 8;
             byte rotation = (byte) (((facing % 16) + 16) % 16);
-            skull.setRotation(GlowSkull.getRotation(rotation));
+            skull.setRotation(getRotation(rotation));
         }
         skull.update();
     }
@@ -78,4 +84,97 @@ public class BlockSkull extends BlockType {
 
         return Arrays.asList(drop);
     }
+
+    public static PlayerProfile getProfile(String name) {
+        if(name == null || name.length() > MAX_OWNER_LENGTH || name.isEmpty()) return null;
+
+        UUID uuid = ((GlowServer) Bukkit.getServer()).getPlayerDataService().lookupUUID(name);
+        return new PlayerProfile(name, uuid);
+    }
+
+    public static BlockFace getRotation(byte rotation) {
+        switch (rotation) {
+            case 0:
+                return BlockFace.NORTH;
+            case 1:
+                return BlockFace.NORTH_NORTH_EAST;
+            case 2:
+                return BlockFace.NORTH_EAST;
+            case 3:
+                return BlockFace.EAST_NORTH_EAST;
+            case 4:
+                return BlockFace.EAST;
+            case 5:
+                return BlockFace.EAST_SOUTH_EAST;
+            case 6:
+                return BlockFace.SOUTH_EAST;
+            case 7:
+                return BlockFace.SOUTH_SOUTH_EAST;
+            case 8:
+                return BlockFace.SOUTH;
+            case 9:
+                return BlockFace.SOUTH_SOUTH_WEST;
+            case 10:
+                return BlockFace.SOUTH_WEST;
+            case 11:
+                return BlockFace.WEST_SOUTH_WEST;
+            case 12:
+                return BlockFace.WEST;
+            case 13:
+                return BlockFace.WEST_NORTH_WEST;
+            case 14:
+                return BlockFace.NORTH_WEST;
+            case 15:
+                return BlockFace.NORTH_NORTH_WEST;
+        }
+        throw new IllegalArgumentException("Not a valid skull rotation: " + rotation);
+    }
+
+    public static byte getRotation(BlockFace rotation) {
+        switch(rotation) {
+            case SOUTH:
+                return 0x0;
+            case SOUTH_SOUTH_WEST:
+                return 0x1;
+            case SOUTH_WEST:
+                return 0x2;
+            case WEST_SOUTH_WEST:
+                return 0x3;
+            case WEST:
+                return 0x4;
+            case WEST_NORTH_WEST:
+                return 0x5;
+            case NORTH_WEST:
+                return 0x6;
+            case NORTH_NORTH_WEST:
+                return 0x7;
+            case NORTH:
+                return 0x8;
+            case NORTH_NORTH_EAST:
+                return 0x9;
+            case NORTH_EAST:
+                return 0xA;
+            case EAST_NORTH_EAST:
+                return 0xB;
+            case EAST:
+                return 0xC;
+            case EAST_SOUTH_EAST:
+                return 0xD;
+            case SOUTH_EAST:
+                return 0xE;
+            case SOUTH_SOUTH_EAST:
+                return 0xF;
+        }
+        throw new IllegalArgumentException("Not a valid skull rotation:" + rotation);
+    }
+
+    public static SkullType getType(int id) {
+        if(id >= SkullType.values().length || id < 0) throw new IllegalArgumentException("ID not a Skull type: " + id);
+        return SkullType.values()[id];
+    }
+
+    public static byte getType(SkullType type) {
+        return (byte) type.ordinal();
+    }
+
 }
--- src/main/java/net/glowstone/block/state/GlowSkull.java
+++ src/main/java/net/glowstone/block/state/GlowSkull.java
@@ -2,9 +2,9 @@ package net.glowstone.block.state;
 
 import net.glowstone.block.GlowBlock;
 import net.glowstone.block.GlowBlockState;
+import net.glowstone.block.blocktype.BlockSkull;
 import net.glowstone.block.entity.TESkull;
-import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
+import net.glowstone.entity.meta.PlayerProfile;
 import org.bukkit.SkullType;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.Skull;
@@ -12,13 +12,13 @@ import org.bukkit.block.Skull;
 public class GlowSkull extends GlowBlockState implements Skull {
 
     private SkullType type;
-    private OfflinePlayer owner;
+    private PlayerProfile owner;
     private BlockFace rotation;
 
     public GlowSkull(GlowBlock block) {
         super(block);
-        type = getType(getTileEntity().getType());
-        rotation = getRotation(getTileEntity().getRotation());
+        type = BlockSkull.getType(getTileEntity().getType());
+        rotation = BlockSkull.getRotation(getTileEntity().getRotation());
         owner = getTileEntity().getOwner();
     }
 
@@ -31,8 +31,8 @@ public class GlowSkull extends GlowBlockState implements Skull {
         boolean result = super.update(force, applyPhysics);
         if(result) {
             TESkull skull = getTileEntity();
-            skull.setType(getType(type));
-            skull.setRotation(getRotation(rotation));
+            skull.setType(BlockSkull.getType(type));
+            skull.setRotation(BlockSkull.getRotation(rotation));
             if(type == SkullType.PLAYER) {
                 skull.setOwner(owner);
             }
@@ -48,14 +48,16 @@ public class GlowSkull extends GlowBlockState implements Skull {
 
     @Override
     public String getOwner() {
-        return owner.getName();
+        return (hasOwner() ? owner.getName() : null);
     }
 
     @Override
     public boolean setOwner(String name) {
-        if(name == null || name.length() > 16) return false;
-
-        this.owner = Bukkit.getOfflinePlayer(name);
+        PlayerProfile owner = BlockSkull.getProfile(name);
+        if(owner == null) {
+            return false;
+        }
+        this.owner = owner;
         this.setSkullType(SkullType.PLAYER);
         return true;
     }
@@ -83,88 +85,4 @@ public class GlowSkull extends GlowBlockState implements Skull {
         this.type = type;
     }
 
-    public static SkullType getType(int id) {
-        if(id >= SkullType.values().length || id < 0) throw new IllegalArgumentException("ID not a Skull type: " + id);
-        return SkullType.values()[id];
-    }
-
-    public static byte getType(SkullType type) {
-        return (byte) type.ordinal();
-    }
-
-    public static BlockFace getRotation(byte rotation) {
-        switch (rotation) {
-            case 0:
-                return BlockFace.NORTH;
-            case 1:
-                return BlockFace.NORTH_NORTH_EAST;
-            case 2:
-                return BlockFace.NORTH_EAST;
-            case 3:
-                return BlockFace.EAST_NORTH_EAST;
-            case 4:
-                return BlockFace.EAST;
-            case 5:
-                return BlockFace.EAST_SOUTH_EAST;
-            case 6:
-                return BlockFace.SOUTH_EAST;
-            case 7:
-                return BlockFace.SOUTH_SOUTH_EAST;
-            case 8:
-                return BlockFace.SOUTH;
-            case 9:
-                return BlockFace.SOUTH_SOUTH_WEST;
-            case 10:
-                return BlockFace.SOUTH_WEST;
-            case 11:
-                return BlockFace.WEST_SOUTH_WEST;
-            case 12:
-                return BlockFace.WEST;
-            case 13:
-                return BlockFace.WEST_NORTH_WEST;
-            case 14:
-                return BlockFace.NORTH_WEST;
-            case 15:
-                return BlockFace.NORTH_NORTH_WEST;
-        }
-        throw new IllegalArgumentException("Not a valid skull rotation: " + rotation);
-    }
-
-    public static byte getRotation(BlockFace rotation) {
-        switch(rotation) {
-            case SOUTH:
-                return 0x0;
-            case SOUTH_SOUTH_WEST:
-                return 0x1;
-            case SOUTH_WEST:
-                return 0x2;
-            case WEST_SOUTH_WEST:
-                return 0x3;
-            case WEST:
-                return 0x4;
-            case WEST_NORTH_WEST:
-                return 0x5;
-            case NORTH_WEST:
-                return 0x6;
-            case NORTH_NORTH_WEST:
-                return 0x7;
-            case NORTH:
-                return 0x8;
-            case NORTH_NORTH_EAST:
-                return 0x9;
-            case NORTH_EAST:
-                return 0xA;
-            case EAST_NORTH_EAST:
-                return 0xB;
-            case EAST:
-                return 0xC;
-            case EAST_SOUTH_EAST:
-                return 0xD;
-            case SOUTH_EAST:
-                return 0xE;
-            case SOUTH_SOUTH_EAST:
-                return 0xF;
-        }
-        throw new IllegalArgumentException("Not a valid skull rotation:" + rotation);
-    }
 }
